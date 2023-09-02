f3dcbfe448c1d7a63fe3e3ef1aa2473c93fc842d
Zishuo Li <fourcases1408@outlook.com>
2023-09-02 00:57:29 -0400

    added expanding cards in angular

---
+++ expanding cards/angular/src/app/app.component.ts
@@ -0,0 +1,47 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'app-root',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss']
+})
+export class AppComponent {
+  title = 'angular';
+  items = items
+  active: number = 0
+
+  onClick(index: number): void {
+    this.active = index
+  }
+}
+
+const items = [
+  {
+    title: 'Explore The World',
+    image:
+      'https://images.unsplash.com/photo-1558979158-65a1eaa08691?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1350&q=80}',
+  },
+  {
+    title: 'Wild Forest',
+    image:
+      'https://images.unsplash.com/photo-1572276596237-5db2c3e16c5d?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1350&q=80',
+  },
+  {
+    title: 'Sunny Beach',
+    image:
+      'https://images.unsplash.com/photo-1507525428034-b723cf961d3e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1353&q=80',
+  },
+  {
+    title: 'City on Winter',
+    image:
+      'https://images.unsplash.com/photo-1551009175-8a68da93d5f9?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1351&q=80',
+  },
+  {
+    title: 'Mountains - Clouds',
+    image:
+      'https://images.unsplash.com/photo-1549880338-65ddcdfd017b?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1350&q=80',
+  },
+]
